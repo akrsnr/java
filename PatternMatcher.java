0b89f55508691ca4a54d13cf91db6cf80e43219f
Soner <sonerakarrr@gmail.com>
2018-06-02 01:41:44 +0300

    Create PatternMatcher.java

---
+++ PatternMatcher.java
@@ -0,0 +1,22 @@
+// Only for rememberance how extraction works with groups java
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class PatternMatcher {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        String input = "foto_20180601_1.jpg\n" +
+                " foto_20180601_10.jpg\n" +
+                " foto_20180601_100.jpg\n" +
+                " foto_20180601_1000.jpg";
+
+        Pattern p = Pattern.compile("foto_\\d{8}_(\\d+)");
+        Matcher m = p.matcher(input);
+
+        while (m.find()) {
+            System.out.println("Found a group(0)" + m.group(0) + " ~~ " + "Found a group(1)" + m.group(1));
+        }
+
+    }
+}
