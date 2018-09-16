0229a4b620c8836762538e93f63856166a3211aa
Soner <sonerakarrr@gmail.com>
2018-09-16 18:07:20 +0300

    Update binaryTreeFindMax.java

--- binaryTreeFindMax.java
+++ binaryTreeFindMax.java
@@ -1,28 +1,31 @@
 // Soner - Exercise
 
-public static int findMax(Node binaryTreeNode) {
-    if (binaryTreeNode.right == null && binaryTreeNode.left == null) {
-        return binaryTreeNode.data;
-    }
-    if (binaryTreeNode.left == null) {
-        if (binaryTreeNode.right.data > binaryTreeNode.data)
-            return binaryTreeNode.right.data;
-        else
+
+public static int findMax(Node binaryTreeNode) throws IllegalAccessException {
+        if (binaryTreeNode == null) throw new IllegalAccessException("Passed null reference");
+        if (binaryTreeNode.right == null && binaryTreeNode.left == null) {
             return binaryTreeNode.data;
-    }
-    if (binaryTreeNode.right == null) {
-        if (binaryTreeNode.left.data > binaryTreeNode.data)
-            return binaryTreeNode.left.data;
-        else
-            return binaryTreeNode.data;
-    }
+        }
+        if (binaryTreeNode.left == null) {
+            if (binaryTreeNode.right.data > binaryTreeNode.data)
+                return binaryTreeNode.right.data;
+            else
+                return binaryTreeNode.data;
+        }
+        if (binaryTreeNode.right == null) {
+            if (binaryTreeNode.left.data > binaryTreeNode.data)
+                return binaryTreeNode.left.data;
+            else
+                return binaryTreeNode.data;
+        }
     
-    // if both branches are not null
+        // if both branches are not null
 
-    int leftData = findMax(binaryTreeNode.left);
-    int righData = findMax(binaryTreeNode.right);
 
-    int maxOfTwoBranches = leftData > righData ? leftData : righData;
+        int leftData = findMax(binaryTreeNode.left);
+        int righData = findMax(binaryTreeNode.right);
 
-    return binaryTreeNode.data > maxOfTwoBranches ? binaryTreeNode.data : maxOfTwoBranches;
-}
+        int maxOfTwoBranches = leftData > righData ? leftData : righData;
+
+        return binaryTreeNode.data > maxOfTwoBranches ? binaryTreeNode.data : maxOfTwoBranches;
+    }
